65842909289c39b9f78c398b095daa07113eb2e5
davidrk <david_knotten@hotmail.no>
2023-02-01 13:49:11 +0100

    Print class dict

--- imageloader.py
+++ imageloader.py
@@ -5,18 +5,21 @@ from os import listdir
 
 class ImageLoader:
 
-    def read_paths_and_classify(self, base_path: str = './data/asl_alphabet_train/asl_alphabet_train'):
+    def read_paths_and_classify(base_path: str = './data/asl_alphabet_train/asl_alphabet_train'):
         """
         Create all
         :param base_path:
         :return:
         """
+        classes = dict()
         images = []
         class_id = 0
         for c in listdir(base_path):
             class_id += 1
+            classes[c] = class_id
             for file in listdir(base_path + "/" + c):
                 images.append([base_path + "/" + c + "/" + file, class_id])
+        print(classes)
         return images
 
     def load_image_from_path(self, path: str):
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import torch as tr
 import os.path as path
 from os import listdir
+from imageloader import ImageLoader
 
 img = cv.imread("./data/asl_alphabet_train/asl_alphabet_train/A/A1.jpg", cv.IMREAD_GRAYSCALE)
 # cv.imshow("Window", img)
@@ -14,26 +15,5 @@ print(img)
 files = 0
 
 
-def read_paths_and_classify(base_path: str = './data/asl_alphabet_train/asl_alphabet_train'):
-    """
-    Create all 
-    :param base_path:
-    :return:
-    """
-    images = []
-    class_id = 0
-    for c in listdir(base_path):
-        class_id += 1
-        for file in listdir(base_path + "/" + c):
-            images.append([base_path + "/" + c + "/" + file, class_id])
-    return images
-
-
-def load_image_from_path(path: str):
-    img = cv.imread(path, cv.IMREAD_GRAYSCALE)
-    img = np.array(img).flatten()
-    return img
-
-
-print(read_paths_and_classify())
+ImageLoader.read_paths_and_classify()
 
