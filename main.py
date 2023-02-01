cd0ac1f3a7f74bcc075e68e14cd05f8c03d9ed7c
olerid <olerid02@gmail.com>
2023-02-01 13:27:17 +0100

    test change

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from os import listdir
 img = cv.imread("./data/asl_alphabet_train/asl_alphabet_train/A/A1.jpg", cv.IMREAD_GRAYSCALE)
 # cv.imshow("Window", img)
 # cv.waitKey(0) & 0xFF
+# Test comment
 
 img = np.array(img).flatten()
 print(img)
