06df41ae63a8783a91a7ea5f5e0b8c98b47d0487
olerid <olerid02@gmail.com>
2023-02-08 09:35:27 +0100

    Created PyTorch dataset for images

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import cv2 as cv
 import torch as tr
 import os.path as path
 from os import listdir
-from imageloader import ImageLoader
+from imagepathloader import ImagePathLoader
+from imagedataset import ImageDataset
 import matplotlib.pyplot as plt
 
 # cv.imshow("Window", img)
 # cv.waitKey(0) & 0xFF
 
 if __name__ == "__main__":
-    paths = ImageLoader.read_paths_and_classify()
-    print(np.array(paths).shape)
+    data = ImageDataset()
+    print(data[0])
