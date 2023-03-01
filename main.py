bff81f900d0c907cdc5f27861096df3b6532d100
olerid <olerid02@gmail.com>
2023-03-01 13:25:42 +0100

    Updated predictor function slightly

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import numpy as np
 import cv2 as cv
-import torch as tr
+import torch
 import os.path as path
 from os import listdir
 import matplotlib.pyplot as plt
 
 from image_datasets.imagedataset import ImageDataset
 from models.dropoutModel import DropoutModel
-from predictor import predict
+from predictor import predict, load_model
 
 # cv.imshow("Window", img)
 # cv.waitKey(0) & 0xFF
 
 def main():
+    load_model(model:=DropoutModel(), "./models/saved/model_dropout_v3.pth")
+
     images = ImageDataset()
     image = images[0][0]
-    print(image.shape)
+    print(image)
 
-    pred = predict(DropoutModel(), "./models/saved/model_dropout_v3.pth", images=image)
+    pred = predict(model, images=image)
     print(pred)
-
 
 if __name__ == "__main__":
     main()
--- predictor.py
+++ predictor.py
@@ -3,22 +3,38 @@ import torch.nn as nn
 import numpy as np
 
 
-def predict(model: nn.Module, model_path: str, images: torch.Tensor, device = "cpu") -> torch.tensor:
+def load_model(model: nn.Module, model_path) -> None:
+    """
+    Given a blank model of the desired type, and a path to the dictionary it
+    is saved to, loads the model parameters into the model INPLACE and calls
+    .eval().
+
+    Args:
+        model (nn.Module): A blank (untrained) model of the kind you want to load
+        model_path  (str): Path to saved model
+    Returns:
+        Trained PyTorch model
+    """
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.load_state_dict(
+        torch.load(
+        model_path, map_location=device)
+    )
+    model.eval()
+
+
+def predict(model: nn.Module, images: torch.Tensor) -> torch.tensor:
     """
     Given the path to a trained PyTorch model, and one or more images to
     predict, returns predictions for all images.
 
     Args:
-        model       (nn.Module): A blank (untrained) model of the kind you want to load
-        model_path        (str): Path to saved model
+        model       (nn.Module): Trained PyTorch model
         images (PyTorch tensor): Pytorch tensor of one or more images to predict
     Returns:
         Predictions for each image in raw format
     """
-    model.load_state_dict(
-        torch.load(model_path, map_location=device)
-    )
-    model.eval()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     images = images.float().to(device)
     with torch.no_grad():
         return model(images)
