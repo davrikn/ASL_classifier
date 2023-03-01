a053160c4bb4c74675a48e77bba4b65d7085f3a6
Øyvind <oyvinhha@stud.ntnu.no>
2023-03-01 10:46:24 +0100

    updating GUI_CV again

--- GUI_CV.py
+++ GUI_CV.py
@@ -8,6 +8,46 @@ import random
 import numpy as np
 from matplotlib import cm
 import copy
+import dropoutModel
+import torch
+import numpy.ma as ma
+
+
+def change_format(frame):#Redundant function to change format of image array. Use transpose instead...
+    """Changes format from webcam: [[[r,g,b],[r,g,b]][[r,g,b]]]
+    to correct format: [[[r,200,r],[r,200,r]], [[b,200,b],[b,200,b]], [[g,200,c],...,[c,200,c]]]"""
+    r_verdier=[]
+    g_verdier=[]
+    b_verdier=[]
+    r_verdier_temp=[]
+    g_verdier_temp=[]
+    b_verdier_temp=[]
+    counter=0
+    g_counter=0
+    b_counter=0
+    for i in range(len(frame)):
+        for j in range(len(frame[i])):
+
+            r_verdier_temp.append(frame[i][j][0])
+            g_verdier_temp.append(frame[i][j][1])
+            b_verdier_temp.append(frame[i][j][2])
+            counter+=1
+            if counter==200:#200
+                r_verdier.append(r_verdier_temp)
+                g_verdier.append(g_verdier_temp)
+                b_verdier.append(b_verdier_temp)
+                counter=0
+                r_verdier_temp=[]
+                g_verdier_temp=[]
+                b_verdier_temp=[]
+
+    final=[[],[],[]]
+    for i in range(len(r_verdier)):#number of lists?
+        final[0].append(r_verdier[i])
+        final[1].append(g_verdier[i])
+        final[2].append(b_verdier[i])
+
+    return final
 
 class App:
     def __init__(self, window, window_title, video_source=0):
@@ -36,9 +76,10 @@ class App:
         self.output_text.pack()
 
         # After it is called once, the update method will be automatically called every delay milliseconds
-        self.delay = 150
+        self.delay = 15
         #window.update_idletasks()
         #print("updated")
+        self.model=dropoutModel.DropoutModel()
 
         self.update()
         self.window.mainloop()
@@ -46,10 +87,6 @@ class App:
     def snapshot(self):
         # Get a frame from the video source
         ret, frame = self.vid.get_frame()
-        #print("frame",frame)
-        #print(" is :::",len(frame))
-        #print("[] is:::",len(frame[0]))
-        #print("ret",ret)
 
         if ret:
             cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
@@ -64,27 +101,35 @@ class App:
             frame2=copy.copy(frame)
             frame2=frame2
             im = PIL.Image.fromarray(np.array(frame2).astype("uint8"))
-            #print("im_type",type(im))
 
             #a=PIL.Image.open('frame-08-02-2023-15-38-51.jpg')
             a=im
             print("TYPE:",type(a))
             width,height=a.size
-            #print("width",width)
-            #print("height",height)
-            #im1 = im.crop((left, top, right, bottom))
-            #80,480,560,0
             im1 = a.crop(((width-height)/2, 0, width-((width-height)/2), height))
-            #im1 = a.crop((1, 1, 200,300))
             im1.save('image_400_lol.jpg')
             new_image = im1.resize((200, 200))
             new_image.save('image_400.jpg')
             self.i+=1
-        prediction=random.randint(0,10)#exchange with actual prediction from network
-        self.output_text.config(text = str(prediction))
-            
 
+        frameA=np.array(frame)
+        frameA=frameA.transpose(2,0,1)
+        frame3=frameA.tolist()
+        """
+        for i in range(len(frame3)):
+            for j in range(len(frame3[i])):
+                for k in range(len(frame3[i][j])):
+                    #print("k:",k)
+                    frame3[i][j][k]=float(frame3[i][j][k])"""
+        frameq=np.array(frame3,dtype=int)
+        #frameq=np.array(frame3,dtype=float)
+        frame_changed=torch.tensor(frameq)
+        prediction=self.model.predict(frame_changed)               #remove #
+
+        self.output_text.config(text = str(prediction))               #remove #
+            
         if ret:
+            frame=cv2.flip(frame,1)
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
             self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
 
