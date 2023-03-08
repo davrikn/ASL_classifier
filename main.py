097affe12d669512498a4ded88900b274a33924c
olerid <olerid02@gmail.com>
2023-03-08 10:18:07 +0100

    No major changes

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ from tests.test_get_emission_probabilities import test_get_emission_probabilitie
 # cv.waitKey(0) & 0xFF
 
 def main():
-    test_get_emission_probabilities(N=100)
+    # test_get_emission_probabilities(N=100)
+    images = ImageDataset()
+    images[0]
 
 if __name__ == "__main__":
     main()
