23a849d36780bf2c7bdcb294eb624742c45166d7
davidrk <david_knotten@hotmail.no>
2023-03-15 10:40:40 +0100

    Slash blue edges and normalize images

--- models/dropoutModel.py
+++ models/dropoutModel.py
@@ -10,7 +10,7 @@ class DropoutModel(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(128 * 25 * 25, 512)
+        self.fc1 = nn.Linear(128 * 24 * 24, 512) # 192/2^-3 = 24
         self.dropout1 = nn.Dropout(p=0.1)
         self.fc2 = nn.Linear(512, 29)
 
@@ -18,7 +18,7 @@ class DropoutModel(nn.Module):
         x = self.pool(torch.relu(self.conv1(x)))
         x = self.pool(torch.relu(self.conv2(x)))
         x = self.pool(torch.relu(self.conv3(x)))
-        x = x.view(-1, 128 * 25 * 25)
+        x = x.view(-1, 128 * 24 * 24)
         x = torch.relu(self.fc1(x))
         x = self.dropout1(x)
         x = self.fc2(x)
