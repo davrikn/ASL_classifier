53d4d30d251bcf376c2b6c5b8eb400ecb70592d1
Øyvind <oyvinhha@stud.ntnu.no>
2023-03-15 10:17:02 +0100

    updating GUI_CV2.py

--- GUI_CV2.py
+++ GUI_CV2.py
@@ -34,6 +34,7 @@ class App:
         self.window.title(window_title)
         self.video_source = video_source
         self.i=0
+        self.aaaaaa=0
         self.last_times=[]
 
         greeting = tkinter.Label(text="ASL Sign Language Detection!",font=("Arial", 25))
@@ -91,12 +92,10 @@ class App:
         self.window.mainloop()
     
     def openNewWindow(self):
-        # Toplevel object which will
-        # be treated as a new window
+        # Toplevel object which will be treated as a new window
         newWindow = Toplevel(self.window)
     
-        # sets the title of the
-        # Toplevel widget
+        # sets the title of the Toplevel widget
         newWindow.title("New Window")
     
         # sets the geometry of toplevel
@@ -134,9 +133,14 @@ class App:
             cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
 
     def update(self):
+        time_consumption=[]
+        if self.aaaaaa==1:
+            self.time_list=time_consumption
+            print(self.time_list)
         # Get a frame from the video source
         ret, frame = self.vid.get_frame()
         now = time.time()
+        time_consumption.append(time.time())
         if len(self.last_times) == 20:
             self.last_times.append(round(now-self.last_time,3))
             self.last_times.pop(0)
@@ -145,21 +149,27 @@ class App:
                 self.last_times.append(round(now-self.last_time,3))
             except:
                 useless_variable=1
-
+        time_consumption.append(time.time())
         #Converting to PIL
         #frame=PIL.Image.open("C:/Users/Øyvind/Desktop/ASL_sign_language/ASL_classifier/data/asl_alphabet_train/A/A2.jpg")       #testing on image A2 works.
         im = PIL.Image.fromarray(np.array(frame).astype("uint8"))
-
+        time_consumption.append(time.time())
         #Cropping to 200x200
         width,height=im.size
         im = im.crop(((width-height)/2, 0, width-((width-height)/2), height))
+        time_consumption.append(time.time())
         im = im.resize((200, 200))
+        time_consumption.append(time.time())
         #im.save("aaaaaaaaaa.jpg")
         #Transposing and transforming into a tensor
         im=np.array(im)
+        time_consumption.append(time.time())
         copied=copy.copy(im)
+        time_consumption.append(time.time())
         copied2=copied.transpose(2,0,1)
+        time_consumption.append(time.time())
         copied2=torch.tensor(copied2)
+        time_consumption.append(time.time())
 
                                             #Ignore these lines.
         #Previously had and issue with data types, so I looped through and converted to float. Works without.
@@ -174,11 +184,16 @@ class App:
 
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         #Make prediction
-        prediction=predict(self.model,"C:/Users/Øyvind/Desktop/ASL_sign_language/ASL_classifier/model_dropout_v3.pth",copied2,device=device)
+        #prediction=predict(self.model,"C:/Users/Øyvind/Desktop/ASL_sign_language/ASL_classifier/model_dropout_v3.pth",copied2,device=device)
+        prediction=predict(self.model,copied2)
+        time_consumption.append(time.time())
         #Display output from prediction
         best=torch.argmax(prediction)
+        time_consumption.append(time.time())
         softmax_obj=torch.nn.Softmax(dim=1)
+        time_consumption.append(time.time())
         softmax=softmax_obj(prediction)
+        time_consumption.append(time.time())
         #print("type of softmax is:",(softmax))
         tall=chr(best+65)
         ting=softmax[0][best]
@@ -190,24 +205,32 @@ class App:
             self.output_text.config(text = str(tall)+" Med sannsynlighet: "+str(ting.item())+"\n Time since last frame:"+str(now-self.last_time)+"\n Last 10 frame-times:"+str(self.last_times))
         except:
             useless_variable=1
-
+        time_consumption.append(time.time())
         self.ax.cla()
         self.ax.plot(self.last_times)
  
         self.canvas2.draw()
         self.canvas2.get_tk_widget().pack(fill=tkinter.BOTH, expand=True)
-            
+        time_consumption.append(time.time())
         if ret:
             frame=cv2.flip(frame,1)
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
             self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+        time_consumption.append(time.time())
+        self.aaaaaa+=1
 
+        #Prints time consumption of various steps in code
+        output_thing=[]
+        for i in range(len(time_consumption)):
+            output_thing.append(time_consumption[i]-time_consumption[0])
+        print("time_consumption",output_thing)
         #try:
         #    self.last_times.append(round(now-self.last_time,3))
         #except:
         #    useless_variable=1
         self.last_time=now
         self.window.after(self.delay, self.update)
+        
 
 
 class MyVideoCapture:
